4c56eefb7cef705421b8afc2f10b29903e0d4ffa
IrinaMai <irinam3340@gmail.com>
2020-12-09 18:35:38 +0300

    Class done

--- src/index.js
+++ src/index.js
@@ -1,47 +1,53 @@
 import './styles.css';
 
-const refs = {
-    days: document.querySelector('[data-value="days"]'),
-    hours: document.querySelector('[data-value="hours"]'),
-    mins: document.querySelector('[data-value="mins"]'),
-    secs: document.querySelector('[data-value="secs"]'),
-    selector:document.querySelector('#timer-1'),
-};
 
 
-
-const countDownTimer = {
-    finish() {
-        const finalDate = new Date(2021, 0, 11).getTime()
+class CountdownTimer {
+    constructor({selector, targetDate}) {
+        this.timer = document.querySelector(selector);
+        this.targetDate = targetDate.getTime(); 
+        this.days = "";
+        this.hours = "";
+        this.mins = "";
+        this.secs = "";
+    }
+    getCurrentTime() {
+        return new Date().getTime();
+    }
+    daysCalculator() {
         setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = finalDate - currentTime;
-            daysCalculator(deltaTime);
-        }, 1000);
-    },
-    
-};
-
-countDownTimer.finish();
-
-
-function daysCalculator(time) {
-    const days = String(Math.floor(time/ (1000 * 60 * 60 * 24))).padStart(3,'0');
-    const hours = modify(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = modify(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = modify(Math.floor((time % (1000 * 60)) / 1000));
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.mins.textContent = mins;
-    refs.secs.textContent = secs;
+            let time = this.targetDate - this.getCurrentTime();
+            this.days= Math.floor(time / (1000 * 60 * 60 * 24));
+            this.hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            this.mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+            this.secs = Math.floor((time % (1000 * 60)) / 1000);
+            this.displayData();
+        }, 1000)
+    }
+    displayData() {
+        this.daysRef = this.timer.querySelector('[data-value="days"]').textContent = this.days;
+        this.hoursRef = this.timer.querySelector('[data-value="hours"]').textContent = this.hours;
+        this.minsref = this.timer.querySelector('[data-value="mins"]').textContent = this.mins;
+        this.secsRef = this.timer.querySelector('[data-value="secs"]').textContent = this.secs;
+    }
+ 
+   
 };
 
 
-const modify = (value) => {
-   return String(value).padStart(2, "0")
-}
+
+const timer1 = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2021'),
+});
+timer1.daysCalculator();
 
 
 
+const myBirthDay = new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('jan 11, 2021'),
+});
+myBirthDay.daysCalculator();
 
 
